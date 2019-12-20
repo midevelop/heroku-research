bc0a238641cb56666977196963d6ca9f4787bde4
midevelop <mi@elbrusboot.camp>
2019-12-20 12:18:10 +0300

    sdad

--- back/app.js
+++ back/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   res.status(200).json({api: 'version 1'})
