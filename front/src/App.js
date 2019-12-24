1780308ae4efceef75d05f676194b2d39dd15722
midevelop <mi@elbrusboot.camp>
2019-12-24 11:42:16 +0300

    feat(front): proxy server added

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+async function test () {
+  const resp = await fetch("/api")
+  console.log(resp)
+
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">My header
+    <button onClick={test}>send fetch</button>
     </div>
   );
 }
